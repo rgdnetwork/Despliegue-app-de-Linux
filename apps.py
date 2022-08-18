159b8160384ddb3039d7015bcb3ea791bca51469
rgdnetwork <98336465+rgdnetwork@users.noreply.github.com>
2022-08-18 15:45:49 +0100

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,67 @@
+import signal
+from time import sleep
+import os
+
+print("Bienvenido a mi instalador")
+sleep(2) 
+print("A continuación deberá de elegir un número para instalar el programa deseado en su máquina")
+sleep(2)
+
+def mostrar_menu(opciones):
+    print('Seleccione una opción:')
+    for clave in sorted(opciones):
+        print(f' {clave}) {opciones[clave][0]}')
+
+
+def leer_opcion(opciones):
+    while (a := input('Opción: ')) not in opciones:
+        print('Opción incorrecta, vuelva a intentarlo.')
+    return a
+
+
+def ejecutar_opcion(opcion, opciones):
+    opciones[opcion][1]()
+
+
+def generar_menu(opciones, opcion_salida):
+    opcion = None
+    while opcion != opcion_salida:
+        mostrar_menu(opciones)
+        opcion = leer_opcion(opciones)
+        ejecutar_opcion(opcion, opciones)
+        print()
+
+
+def menu_principal():
+    opciones = {
+        '1': ('JDK (Java Development Kit)', accion1),
+        '2': ('Opción 2', accion2),
+        '3': ('Opción 3', accion3),
+        '4': ('Salir', salir)
+    }
+
+    generar_menu(opciones, '4')
+
+
+def accion1():
+    print('Has elegido la opción 1')
+    sleep(2)
+    print("Descargando JDK")
+    sleep(2)
+    os.system("apt update && apt upgrade -y && apt-get install openjdk-17-jdk && java --version")
+
+
+def accion2():
+    print('Has elegido la opción 2')
+
+
+def accion3():
+    print('Has elegido la opción 3')
+
+
+def salir():
+    print('Saliendo')
+
+
+if __name__ == '__main__':
+    menu_principal()
